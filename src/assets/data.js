dbd68c496eedda7ee38e4ede4c503a8f9b333c77
Camy-Cancela <camilacancela11@gmail.com>
2024-01-17 17:27:28 -0300

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,23 @@
+// src/assets/data.js
+export const data = [
+  {
+    id: 1,
+    imgUrl: "./assets/images/IMG1.jpg",
+  },
+  {
+    id: 2,
+    imgUrl: "./assets/images/IMG2.jpg",
+  },
+  {
+    id: 3,
+    imgUrl: "./assets/images/IMG3.jpg",
+  },
+  {
+    id: 4,
+    imgUrl: "./assets/images/IMG4.jpg",
+  },
+  {
+    id: 5,
+    imgUrl: "./assets/images/IMG5.jpg",
+  },
+];
